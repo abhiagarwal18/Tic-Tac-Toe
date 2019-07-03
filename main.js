f7ad219907565f63feaf6d383611fcb94884e29e
abhiagarwal18 <itsagarwal18@gmail.com>
2019-07-03 09:56:22 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let counter =0;
 function task(val)
-{   if (document.getElementById(val).innerHTML!="?")
+{   if (document.getElementById(val).innerHTML !="?")
 {
     return false;
 }
@@ -19,5 +19,58 @@ function task(val)
         }
 
     }
+    won()
 
+}
+
+function restart(){
+    var ids = ["1","2","3","4","5","6","7","8","9"];
+    ids.map((data,i)=>{
+        document.getElementById(data).innerHTML="?"
+    })
+}
+
+
+function won(){
+    if((document.getElementById("1").innerHTML == "X" &&
+    document.getElementById("2").innerHTML == "X" &&
+    document.getElementById("3").innerHTML == "X")||(document.getElementById("4").innerHTML == "X" &&
+    document.getElementById("5").innerHTML == "X" &&
+    document.getElementById("6").innerHTML == "X")||(document.getElementById("7").innerHTML == "X" &&
+    document.getElementById("8").innerHTML == "X" &&
+    document.getElementById("9").innerHTML == "X")||(document.getElementById("1").innerHTML == "X" &&
+    document.getElementById("4").innerHTML == "X" &&
+    document.getElementById("7").innerHTML == "X")||(document.getElementById("2").innerHTML == "X" &&
+    document.getElementById("5").innerHTML == "X" &&
+    document.getElementById("8").innerHTML == "X")||(document.getElementById("3").innerHTML == "X" &&
+    document.getElementById("6").innerHTML == "X" &&
+    document.getElementById("9").innerHTML == "X")||(document.getElementById("1").innerHTML == "X" &&
+    document.getElementById("5").innerHTML == "X" &&
+    document.getElementById("9").innerHTML == "X")||(document.getElementById("3").innerHTML == "X" &&
+    document.getElementById("5").innerHTML == "X" &&
+    document.getElementById("7").innerHTML == "X")){
+        alert("X won")
+        restart();
+    }
+
+    else if((document.getElementById("1").innerHTML == "X" &&
+    document.getElementById("2").innerHTML == "0" &&
+    document.getElementById("3").innerHTML == "0")||(document.getElementById("4").innerHTML == "0" &&
+    document.getElementById("5").innerHTML == "0" &&
+    document.getElementById("6").innerHTML == "0")||(document.getElementById("7").innerHTML == "0" &&
+    document.getElementById("8").innerHTML == "0" &&
+    document.getElementById("9").innerHTML == "0")||(document.getElementById("1").innerHTML == "0" &&
+    document.getElementById("4").innerHTML == "0" &&
+    document.getElementById("7").innerHTML == "0")||(document.getElementById("2").innerHTML == "0" &&
+    document.getElementById("5").innerHTML == "0" &&
+    document.getElementById("8").innerHTML == "0")||(document.getElementById("3").innerHTML == "0" &&
+    document.getElementById("6").innerHTML == "0" &&
+    document.getElementById("9").innerHTML == "0")||(document.getElementById("1").innerHTML == "0" &&
+    document.getElementById("5").innerHTML == "0" &&
+    document.getElementById("9").innerHTML == "0")||(document.getElementById("3").innerHTML == "0" &&
+    document.getElementById("5").innerHTML == "0" &&
+    document.getElementById("7").innerHTML == "0")){
+        alert("0 won")
+        restart();
+    }
 }
